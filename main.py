3b11d06ec03cb88cf83285a56c7ea8e590c026b4
Adriana <piccioneadriana@gmail.com>
2025-11-14 14:55:19 +0100

    modifica app.run()

--- main.py
+++ main.py
@@ -169,7 +169,11 @@ def submit():
     )
 
 def main():
-    app.run(port=int(os.environ.get('PORT', 8080)))
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get('PORT', 8080))
+    )
+
 
 if __name__ == "__main__":
     main()
