0d7b438546beb23e0eac75eba74bb36f37c09064
Adriana <piccioneadriana@gmail.com>
2026-03-31 15:40:57 +0200

    modfica def index

--- main.py
+++ main.py
@@ -168,12 +168,14 @@ def submit():
         mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     )
 
-def main():
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get('PORT', 8080))
-    )
+app = Flask(__name__)
+
+# def main():
+#     app.run(
+#         host="0.0.0.0",
+#         port=int(os.environ.get('PORT', 8080))
+#     )
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
